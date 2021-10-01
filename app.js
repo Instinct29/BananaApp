a932298cc199be86dfe8fe498e2ae628cce98e64
Instinct29 <89262248+Instinct29@users.noreply.github.com>
2021-10-01 19:20:59 +0530

    console.logging the translation
    doing it on console.log

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var clicktxt = document.querySelector("#btn-translate");
 var textinput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output-div");
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url = "https://api.funtranslations.com/translate/minion.json"
 
 
 function gettranslationurl(text){
@@ -14,7 +15,9 @@ function clicked(){
      var inputtext = textinput.value;
      fetch(gettranslationurl(inputtext))
          .then(response => response.json())
-         .then(json =>console.log(json));
+         .then(json => {
+             outputdiv.innerText= json.contents.translated
+              });
 
 
 };
