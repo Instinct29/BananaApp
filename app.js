ee9414a7c71badbdce134a26f32b11b1f320f052
Instinct29 <89262248+Instinct29@users.noreply.github.com>
2021-10-01 19:01:17 +0530

    fetching done
    url
    fetch(url) .then().then()

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var clicktxt = document.querySelector("#btn-translate");
 var textinput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output-div");
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+
+function gettranslationurl(text){
+    return url + "?" +  "text=" +text;
+}
+
 
 function clicked(){
-    outputdiv.innerText = "nahtnam ruog" + textinput.value;
-}
+    // outputdiv.innerText = "nahtnam ruog" + textinput.value;
+     var inputtext = textinput.value;
+     fetch(gettranslationurl(inputtext))
+         .then(response => response.json())
+         .then(json =>console.log(json));
+
+
+};
 clicktxt.addEventListener("click",clicked);
+
